9c76784bc17eb022e4dd3a64640d92365bfb02ab
Eric-Liu1 <liu01619@umn.edu>
2024-03-19 23:10:52 -0500

    bug

--- src/main/java/com/ex/dualblog/DualblogApplication.java
+++ src/main/java/com/ex/dualblog/DualblogApplication.java
@@ -1,13 +1,30 @@
 package com.ex.dualblog;
 
+// import javax.sql.DataSource;
+
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.core.io.ClassPathResource;
+// import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+// import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 
 @SpringBootApplication
+@MapperScan("com.ex.dualblog.mapper")
 public class DualblogApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DualblogApplication.class, args);
 	}
+	// @Bean
+    // public DataSourceInitializer dataSourceInitializer(DataSource dataSource) {
+    //     ResourceDatabasePopulator databasePopulator = new ResourceDatabasePopulator();
+    //     databasePopulator.addScript(new ClassPathResource("mybatis/schema.sql"));
 
+    //     DataSourceInitializer initializer = new DataSourceInitializer();
+    //     initializer.setDataSource(dataSource);
+    //     initializer.setDatabasePopulator(databasePopulator);
+    //     return initializer;
+    // }
 }
---
+++ src/main/java/com/ex/dualblog/mapper/BlogMapper.java
@@ -0,0 +1,12 @@
+package com.ex.dualblog.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.ex.dualblog.model.Blog;
+
+@Mapper
+public interface BlogMapper {
+    List<Blog> getAllBlogs();
+}
