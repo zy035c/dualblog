d507fd2d9d9ca9e6150fc94f2166e3cab528cdc2
Ziyi Chen <zy035c@gmail.com>
2024-04-04 22:00:44 -0400

    user table sql

--- src/main/resources/sql/log.sql
+++ src/main/resources/sql/log.sql
@@ -1,3 +1,15 @@
-CREATE TABLE test(a INT, b VARCHAR(5),c VARCHAR(10));
+CREATE TABLE Blog (         
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,         
+    content TEXT     
+);
 INSERT INTO test VALUES (0,'aaaaa','aaaaaaaaaa'),(1,'bbbbb','bbbbbbbbbb'),(2,'ccccc','cccccccccc');
 SELECT * FROM test;
+
+CREATE TABLE users (
+    id VARCHAR(36) PRIMARY KEY,
+    username VARCHAR(255),
+    phone VARCHAR(26),
+    email VARCHAR(255),
+    password VARCHAR(255)
+);
