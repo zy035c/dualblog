e62ee0c4614f571ffe412aec66897ae433fdf86b
Ziyi Chen <zy035c@gmail.com>
2024-10-08 21:58:08 -0400

    allow cors

--- src/main/java/com/ex/dualblog/config/WebConfig.java
+++ src/main/java/com/ex/dualblog/config/WebConfig.java
@@ -29,7 +29,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:3000") // 允许的来源域名
+            .allowedOrigins("*") // 允许的来源域名
             .allowedMethods("GET", "POST", "PUT", "DELETE") // 允许的 HTTP 方法
             .allowCredentials(true); // 允许携带凭证（例如 Cookie）
     }
